656466325b42b8d3524930b98f069eef57f45d3c
nithil <36654949+nithil@users.noreply.github.com>
2019-01-01 13:26:41 +0530

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -68,10 +68,6 @@ brew install mackup
 echo "Installing Oh My ZSH..."
 curl -L http://install.ohmyz.sh | sh
 
-echo "Setting up Oh My Zsh theme..."
-cd  /Users/bradparbs/.oh-my-zsh/themes
-curl https://gist.githubusercontent.com/bradp/a52fffd9cad1cd51edb7/raw/cb46de8e4c77beb7fad38c81dbddf531d9875c78/brad-muse.zsh-theme > brad-muse.zsh-theme
-
 echo "Setting up Zsh plugins..."
 cd ~/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
